032fe97af3bec8707fa40c6439792728b5a5f3ab
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-01-23 12:28:57 +0100

    render the endpoint controller

--- app/controllers/api/v1/message_controller.rb
+++ app/controllers/api/v1/message_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::MessageController < ApplicationController
+        def index; end
+      
+        def random
+          @message = Message.all.sample
+          render json: { message: @message }
+        end
+
 end
